7e3dfdc85e5a7aa9f715241a0b1aa3d06b5a3529
jpark0506 <peterp1@inha.edu>
2024-11-07 21:35:58 +0900

    feat: 배포 오류 해결

--- iteck-fe/vite.config.ts
+++ iteck-fe/vite.config.ts
@@ -7,10 +7,12 @@ export default defineConfig({
     react(),
   ],
   server: {
+    https: true,
     proxy: {
       "^/api/.*": { // 정규식을 사용하여 정확히 "/api" 경로에 대해서만 프록시 적용
-        target: "https://34.64.87.212:8080",
+        target: "http://34.64.87.212:8080",
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
